1b8c0a8c4bc0038fa16179bb5b17eaaf6478cecc
Daniel Micher <michermd@gmail.com>
2018-01-15 19:05:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,10 +5,6 @@
 
 var recipes = {};
 
-function updateObjectWithKeyAndValue(object, key, value) {
-  return Object.assign({}, object, { [key]: value });
-}
-
 //   describe('updateObjectWithKeyAndValue(object, key, value)', function() {
 //     it('returns an object with the orignal key value pairs and the new key value pair', function() {
 //       var obj = { prop: 1 }
@@ -27,8 +23,8 @@ function updateObjectWithKeyAndValue(object, key, value) {
 //     })
 //   })
 
-function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-  return destructivelyUpdateObjectWithKeyAndValue = Object.assign(object, { [key]: value });
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({}, object, { [key]: value });
 }
 
 // describe('destructivelyUpdateObjectWithKeyAndValue(object, key, value)', function() {
@@ -46,3 +42,41 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 //     })
 //   })
 // })
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  return destructivelyUpdateObjectWithKeyAndValue = Object.assign(object, { [key]: value });
+}
+
+
+describe('deleteFromObjectByKey(object, key)', function() {
+  it('deletes `key` from a clone of object and returns the new object (it is non-destructive)', function() {
+    var obj = { prop: 1 }
+    var newObj = deleteFromObjectByKey(obj, 'prop')
+
+    expect(newObj['prop']).toBe(undefined)
+  })
+
+  it('does not modify the original object (it is non-destructive)', function() {
+    var obj = { prop: 1 }
+
+    deleteFromObjectByKey(obj, 'prop')
+    expect(obj['prop']).toBe(1)
+  })
+})
+
+describe('destructivelyDeleteFromObjectByKey(object, key)', function() {
+  it('returns object without the delete key/value pair', function() {
+    var obj = { prop: 1 }
+    var newObj = destructivelyDeleteFromObjectByKey(obj, 'prop');
+
+    expect(newObj['prop']).toBe(undefined)
+  })
+
+    it('modifies the original object', function() {
+      var obj = { prop: 1 }
+      var newObj = destructivelyDeleteFromObjectByKey(obj, 'prop');
+
+      expect(obj['prop']).toBe(undefined)
+    })
+  })
+})
